48cf8e66989f3e2bc09fbb218849a5568d966332
dyatmika12 <dyatmika12@gmail.com>
2018-11-11 21:54:33 +0800

    tugas migration gusde 1

---
+++ app/Http/Controllers/JurusanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\jurusan;
+use Illuminate\Http\Request;
+
+class JurusanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\jurusan  $jurusan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(jurusan $jurusan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\jurusan  $jurusan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(jurusan $jurusan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\jurusan  $jurusan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, jurusan $jurusan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\jurusan  $jurusan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(jurusan $jurusan)
+    {
+        //
+    }
+}
--- database/migrations/2018_11_11_122102_create_jawabans_table.php
+++ database/migrations/2018_11_11_122102_create_jawabans_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateJawabansTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('jawabans', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('pertanyaan_id');
+            $table->integer('siswa_id');
+            $table->integer('guru_id');
+            $table->text('jawaban');
+            $table->enum('verifikasi', ['0','1']);
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('jawabans');
     }
 }
