defdfc034cc2b1b3103ffae9aec49849e630386e
kertayasaa <iputukerta@gmail.com>
2018-11-12 14:33:24 +0800

    pembaruan 3

--- app/detail_pembayaran.php
+++ app/detail_pembayaran.php
@@ -6,16 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class detail_pembayaran extends Model
 {
-    
-    public function pembayaran(){
-        return $this->belongsTo('App\pembayaran');
-    }
-
-    public function tu(){
-        return $this->hasOne('App\tu');
-    }
 
     public function siswa(){
-        return $this->hasOne('App\siswa');
+        return $this->belongsTo('App\siswa');
     }
 }
